f57e4e516b53d1a620cfdf38d1e415f79ef6de4a
SLain <sl163@ya.ru>
2020-10-12 12:10:58 +0300

    Добавляет кнопку "Результат" (без функционала); Мелкие правки стилией для панели;

--- src/script/components/control-btns.js
+++ src/script/components/control-btns.js
@@ -6,11 +6,18 @@ import {
 const createControlBlock = () => {
     const controlBlock = document.createElement('div');
     const cleanBtn = utils.justBtn('Очистить все');
+    const resultBtn = utils.justBtn('Результат');
 
     controlBlock.classList.add('control-block');
+
     cleanBtn.classList.add('clean-btn');
     cleanBtn.addEventListener('click', () => management.cleanAllData());
+
+    resultBtn.classList.add('result-btn');
+    resultBtn.addEventListener('click', () => console.log('result here'))
+
     controlBlock.append(cleanBtn);
+    controlBlock.append(resultBtn);
 
     return controlBlock;
 }
